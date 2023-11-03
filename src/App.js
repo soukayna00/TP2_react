055eccfcd235cd87ba96eed670ec206484734be0
soukayna00 <soukaynazaidi06@gmail.com>
2023-11-03 16:52:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Slider from './Components/Slider';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
